f3a78096a30acfc9ed3d7de3f967282742e6b7e7
eugustavo <gustavo.ifto@gmail.com>
2020-06-29 22:11:58 -0300

    ✅ Finished Challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,89 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Transactions can only be income or outcome');
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError("You don't have enough balance");
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({ title: category });
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
 export default CreateTransactionService;
+
+// const categoriesRepository = getRepository(Category);
+
+//   const findCategory = categoriesRepository.findOne(title);
+
+//   if (type !== 'income' && type !== 'outcome') {
+//     throw new AppError('Transaction types can only be income or outcome');
+//   }
+
+//   if (!findCategory) {
+//     const newCategory = categoriesRepository.create({ title: category });
+//     await categoriesRepository.save(newCategory);
+
+//     const transaction = transactionsRepository.create({
+//       title,
+//       value,
+//       type,
+//       category_id: newCategory.id,
+//     });
+
+//     await transactionsRepository.save(transaction);
+
+//     return transaction;
+//   }
+
+//   const transaction = transactionsRepository.create({
+//     title,
+//     value,
+//     type,
+//     category_id: findCategory.id,
+//   });
